7fdb45db94b1cd7f4a305de42a1e3602ddbb88a5
shfischer <32540427+shfischer@users.noreply.github.com>
2019-04-29 11:49:53 +0100

    increased grid

--- run_mse.R
+++ run_mse.R
@@ -144,24 +144,27 @@ if (exists("HCRoption")) {
 ### set HCR parameters 
 if (HCRoption %in% 1:6) {
   
-  ### 1-208
+  ### 1-816
   hcr_vals <- expand.grid(
-    Ftrgt = seq(from = 0.1, to = 0.35, by = 0.01),
-    Btrigger = seq(from = 120000, to = 190000, by = 10000))
+    Ftrgt = seq(from = 0.0, to = 0.5, by = 0.01),
+    Btrigger = seq(from = 100000, to = 250000, by = 10000))
+  ### MSY values: 817
+  hcr_vals <- rbind(hcr_vals, data.frame(Ftrgt = 0.194, Btrigger = 132000))
   ### additional combinations after finding yield maximum
-  ### 452-456
-  # comb_max <- switch(HCRoption, 
-  #                    "1" = c(170000, 0.38), 
-  #                    "2" = c(160000, 0.38), 
-  #                    "3" = c(170000, 0.38),
-  #                    "4" = c(190000, 0.40),
-  #                    "5" = c(130000, 0.36),
-  #                    "6" = c(140000, 0.36))
-  # hcr_vals <- rbind(hcr_vals,
-  #                   expand.grid(Ftrgt = c(comb_max[2], comb_max[2]*0.9, 
-  #                                         comb_max[2]*1.1, 0.198, 0.46),
-  #                               Btrigger = comb_max[1]))
-  # 
+  ### max Ftrgt * 1.1 & 0.9, Fmsylower/upper
+  ### 817-822
+  comb_max <- switch(HCRoption,
+                     "1" = c(180000, 0.28),
+                     "2" = c(NA, NA),
+                     "3" = c(180000, 0.28),
+                     "4" = c(NA, NA),
+                     "5" = c(NA, NA),
+                     "6" = c(NA, NA))
+  hcr_vals <- rbind(hcr_vals,
+                    expand.grid(Ftrgt = c(comb_max[2], comb_max[2]*0.9,
+                                          comb_max[2]*1.1, 0.167, 0.194),
+                                Btrigger = comb_max[1]))
+
 }
 
 ### implement
